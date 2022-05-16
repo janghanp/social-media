8b018bdbacd7cfc6ee38f83896ca68658dfc2b4b
Janghan Park <janghan0412@gmail.com>
2022-05-16 20:10:02 +1000

    connect to mongodb via prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
-import type { NextPage } from "next";
+import type { NextPage, GetServerSideProps } from "next";
+
+import { prisma } from "../lib/prisma";
 
 const Home: NextPage = () => {
   return <div className="">home</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const users = await prisma.user.findMany();
+
+  return {
+    props: { users },
+  };
 };
 
 export default Home;
