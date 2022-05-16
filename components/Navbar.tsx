193de98db5bc0df857199db9b7af03695a72e3f2
Janghan Park <janghan0412@gmail.com>
2022-05-16 21:35:01 +1000

    prisma adapter for next-auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div className="w-full border-b border-primary shadow-lg">
@@ -5,14 +7,20 @@ const Navbar = () => {
         <div className="navbar bg-base-100">
           {/* left */}
           {/* logo */}
-          <div className="flex-1">
-            <div className="btn btn-ghost normal-case text-xl">Logo</div>
-          </div>
+          <Link href={"/"}>
+            <div className="flex-1">
+              <div className="btn btn-ghost normal-case text-xl">Logo</div>
+            </div>
+          </Link>
 
           {/* right */}
           <div className="flex-none">
-            <div className="btn btn-ghost">log in</div>
-            <div className="btn btn-outline ml-5">sign up</div>
+            <Link href={"/login"}>
+              <div className="btn btn-ghost">log in</div>
+            </Link>
+            <Link href={"/signup"}>
+              <div className="btn btn-outline ml-5">sign up</div>
+            </Link>
           </div>
         </div>
       </div>
