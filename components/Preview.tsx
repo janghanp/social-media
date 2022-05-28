f67ef804210ca697dbc725284753c1afd413a873
Janghan Park <janghan0412@gmail.com>
2022-05-28 23:20:05 +1000

    PreviewImageItem

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -1,111 +1,66 @@
-import { memo, useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { CustomFile } from "./DropZone";
 import { Pagination, Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import {
   MdOutlineArrowBackIosNew,
   MdOutlineArrowForwardIos,
-  MdClose,
 } from "react-icons/md";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import PreviewImageItem from "./PreviewImageItem";
 
-const Preview = ({
-  files,
-  setFiles,
-}: {
+type Props = {
   files: CustomFile[];
   setFiles: React.Dispatch<React.SetStateAction<CustomFile[]>>;
-}) => {
+};
+
+const Preview = ({ files, setFiles }: Props) => {
   const prevRef = useRef<HTMLDivElement>(null);
   const nextRef = useRef<HTMLDivElement>(null);
 
-  const [currentIndex, setCurrentIndex] = useState<number | null>(null);
-
-  useEffect(() => {
-    //To active next and prev custom buttons
-    setCurrentIndex(0);
-  }, []);
-
-  const deleteHandler = () => {
-    URL.revokeObjectURL(files[currentIndex!].preview);
-
-    setFiles((prevState) => {
-      const updatedFiles = prevState.filter(
-        (_, index) => index !== currentIndex
-      );
-
-      return [...updatedFiles];
-    });
-  };
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   return (
-    <div className="relative z-10 w-full h-full mt-5 rounded-lg overflow-hidden border border-primary">
+    <Swiper
+      className="flex justify-center items-center relative z-10 w-full h-full mt-5 rounded-lg overflow-hidden border border-primary"
+      modules={[Pagination, Navigation]}
+      slidesPerView={1}
+      navigation={{
+        prevEl: prevRef.current!,
+        nextEl: nextRef.current!,
+      }}
+      pagination={{ clickable: true }}
+      onSlideChange={(slide) => {
+        setCurrentIndex(slide.activeIndex);
+      }}
+    >
+      {files.map((file, index) => (
+        <SwiperSlide key={index}>
+          <PreviewImageItem file={file} setFiles={setFiles} />
+        </SwiperSlide>
+      ))}
+      {/* custom prev and next buttons */}
       <div
-        className="btn btn-circle btn-ghost btn-sm absolute z-10 right-3 top-3 bg-black/50 hover:bg-black/30"
-        onClick={deleteHandler}
+        ref={prevRef}
+        className={`btn btn-circle btn-sm btn-ghost absolute top-32 left-3 z-10 text-white bg-black/50 hover:bg-black/30 ${
+          (currentIndex === 0 || !prevRef.current) && "hidden"
+        }`}
       >
-        <MdClose className="w-4 h-4 text-white" />
+        <MdOutlineArrowBackIosNew />
       </div>
-      <Swiper
-        modules={[Pagination, Navigation]}
-        slidesPerView={1}
-        navigation={{
-          prevEl: prevRef.current!,
-          nextEl: nextRef.current!,
-        }}
-        pagination={{ clickable: true }}
-        onSlideChange={(slide) => {
-          setCurrentIndex(slide.activeIndex);
-        }}
+      <div
+        ref={nextRef}
+        className={`btn btn-circle btn-sm btn-ghost absolute top-32 right-3 z-10 text-white bg-black/50 hover:bg-black/30 ${
+          currentIndex === files.length - 1 && "hidden"
+        }`}
       >
-        {files.map((file, index) => {
-          const fileContent = (
-            <>
-              {file.type.includes("video") ? (
-                <>
-                  <video className="w-full h-80 object-cover">
-                    <source src={file.preview} type={file.type} />
-                  </video>
-                </>
-              ) : (
-                <>
-                  <img
-                    className="w-full h-80 object-cover"
-                    src={file.preview}
-                    alt={file.name}
-                  />
-                </>
-              )}
-            </>
-          );
-
-          return (
-            <SwiperSlide key={file.name + index}>{fileContent}</SwiperSlide>
-          );
-        })}
-        {/* custom prev and next buttons */}
-        <div
-          ref={prevRef}
-          className={`btn btn-circle btn-sm btn-ghost absolute top-32 left-3 z-10 text-white bg-black/50 hover:bg-black/30 ${
-            (currentIndex === 0 || !prevRef.current) && "hidden"
-          }`}
-        >
-          <MdOutlineArrowBackIosNew />
-        </div>
-        <div
-          ref={nextRef}
-          className={`btn btn-circle btn-sm btn-ghost absolute top-32 right-3 z-10 text-white bg-black/50 hover:bg-black/30 ${
-            currentIndex === files.length - 1 && "hidden"
-          }`}
-        >
-          <MdOutlineArrowForwardIos />
-        </div>
-      </Swiper>
-    </div>
+        <MdOutlineArrowForwardIos />
+      </div>
+    </Swiper>
   );
 };
 
-export default memo(Preview);
+export default Preview;
