62f60bb882c2286c981bcdfdc3ed4e149d75b51f
Janghan Park <janghan0412@gmail.com>
2022-08-27 23:57:37 +1000

    add profile page

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { HiCog, HiOutlineLogout, HiUserCircle } from 'react-icons/hi';
 
 interface Props {
   image: string;
@@ -14,7 +15,12 @@ const Avatar = ({ image, username, signout }: Props) => {
     document.getElementById('dropdown-menu')?.blur();
   };
 
-  const editProfileHanlder = () => {
+  const settingsHandler = () => {
+    blurDropdown();
+    router.push('/settings');
+  };
+
+  const profileHandler = () => {
     blurDropdown();
     router.push('/profile');
   };
@@ -37,13 +43,22 @@ const Avatar = ({ image, username, signout }: Props) => {
         className="dropdown-content menu rounded-box w-52 border-2 border-primary bg-base-100 p-2 shadow"
       >
         <li>
-          <div className="text-gray-500 hover:cursor-default">@{username}</div>
+          <div onClick={profileHandler}>
+            <HiUserCircle className="h-5 w-5" />
+            <span>Profile</span>
+          </div>
         </li>
         <li>
-          <div onClick={editProfileHanlder}>Profile</div>
+          <div onClick={settingsHandler}>
+            <HiCog className="h-5 w-5" />
+            <span>Setttings</span>
+          </div>
         </li>
         <li>
-          <button onClick={signoutHandler}>Sign Out</button>
+          <button onClick={signoutHandler}>
+            <HiOutlineLogout className="h-5 w-5" />
+            <span>Sign Out</span>
+          </button>
         </li>
       </ul>
     </div>
